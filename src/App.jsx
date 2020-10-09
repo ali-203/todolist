040d286f5d3af66369e05e437f19e66ad3add1d2
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-10 01:18:50 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const App = () => {
-    return
+const [inpuList, setInputList] = useState("buy Apple");
+
+const itemEvent = (event) => {
+setInputList(event.target.value);
+
+};
+const listOfItems = ()
+
+    return (
 <>
-<div>
+
 <div className="main_div">
     <div className="center_div">
     <br />
     <h1> ToDo List </h1>
     <br />
+    <input type= "text" placeholder="Add a Items" onChange={itemEvent} />
+    <button onClick="listOfItems"> + </button>
+    <ol>
+        <li> {inpuList} </li>
+    </ol>
 
     </div>
 </div>
 
-</div>
 
 
-</>;
 
+</>
+    );
 
 };
 export default App;
