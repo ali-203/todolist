e50a23b4e310298af8081907189c2999a4d02515
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-09 23:07:05 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+    return
+<>
+<div>
+<div className="main_div">
+    <div className="center_div">
+    <br />
+    <h1> ToDo List </h1>
+    <br />
+
+    </div>
+</div>
+
+</div>
+
+
+</>;
+
+
+};
+export default App;
